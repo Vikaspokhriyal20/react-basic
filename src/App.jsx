e00f5ba7a99c35fd94fb985a1d3bc7b5a23c3109
vikas singh <pokhriyalvikas940@gmail.com>
2023-08-24 14:06:52 +0530

    useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       <List />
       <Forms />
       <Index />
-      <UseState/>
+      <UseState />
     </>
   )
 }
--- src/Hooks/UseState.jsx
+++ src/Hooks/UseState.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 const UseState = () => {
+  const [count, setCount] = useState(0);
+  const [car, setCar] = useState({
+    brand: 'Ford',
+    model: 'Mustang',
+    color: 'Yellow',
+    year: 1964
+  });
+
+  const increament = () => {
+    setCount((count) => count + 1);
+  }
+
+  const changeColor = () => {
+    setCar(previousState => {
+      return {...previousState , color:'red', year:2021}
+    })
+  }
+
    
   return (
     <>
         <section className='hook-1'>
-              <h4>useState Hook...</h4>
+        <h4>useState Hook...</h4>
+        <h4>Count: {count}</h4>
+        <button onClick={increament}>click me</button>
+
+        <h4>This is my {car.brand} {car.model} in {car.color} color since {car.year}!</h4>
+        <button onClick={changeColor}>change color</button>
       </section>
     </>
   )
